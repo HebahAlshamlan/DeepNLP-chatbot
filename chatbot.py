5f61ef262f2a119220be9de18be32627add4d5fc
Hebah <37045716+HebahAlshamlan@users.noreply.github.com>
2020-01-25 01:05:03 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,3 +5,92 @@ Created on Sat Jan 11 19:46:57 2020
 @author: hebah
 """
 
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 11 19:46:57 2020
+
+@author: hebah
+"""
+
+import numpy as np
+import tensorflow as tf
+import re
+import time
+ 
+ 
+ 
+########## PART 1 - DATA PREPROCESSING ##########
+ 
+ 
+ 
+# Importing the dataset
+lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+ 
+# Creating a dictionary that maps each line and its id
+id2line = {}
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+ 
+# Creating a list of all of the conversations
+conversations_ids = []
+for conversation in conversations[:-1]:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
+    conversations_ids.append(_conversation.split(','))
+ 
+# Getting separately the questions and the answers
+questions = []
+answers = []
+for conversation in conversations_ids:
+    for i in range(len(conversation) - 1):
+        questions.append(id2line[conversation[i]])
+        answers.append(id2line[conversation[i+1]])
+ 
+# Doing a first cleaning of the texts
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"how's", "how is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"n't", " not", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", text)
+    return text
+ 
+# Cleaning the questions
+clean_questions = []
+for question in questions:
+    clean_questions.append(clean_text(question))
+ 
+# Cleaning the answers
+clean_answers = []
+for answer in answers:
+    clean_answers.append(clean_text(answer))
+    
+    
+word2count = {}
+for question in clean_questions:
+    for word in question.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+for answer in clean_answers:
+    for word in answer.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+ 
+ 
